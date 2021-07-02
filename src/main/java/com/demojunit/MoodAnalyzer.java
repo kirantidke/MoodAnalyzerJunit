82c790a633210380ff70fa6e79d2912fb2fa8a9b
kirantidke <kirantidke2013@gmail.com>
2021-07-02 19:33:06 +0530

    "[kiran] refactors code"

--- src/main/java/com/demojunit/MoodAnalyzer.java
+++ src/main/java/com/demojunit/MoodAnalyzer.java
@@ -6,10 +6,25 @@ package com.demojunit;
  */
 public class MoodAnalyzer {
 
-	public String analyseMood(String message) {
-		if (message.toLowerCase().contains("sad"))
-			return "SAD";
-		else
-			return "HAPPY";
-	}
+	 String message;
+
+	    public MoodAnalyzer(String message) {
+	        this.message = message;
+	    }
+	    MoodAnalyzer(){
+	    }
+
+	    public String analyseMood() {
+	        if (message.toLowerCase().contains("sad")){
+	            return "SAD";
+	        }
+	        if (message.toLowerCase().contains("happy")){
+	            return "SAD";
+	        }
+	        if (message.toLowerCase().contains("any")){
+	            return "HAPPY";
+
+	        }else
+	            return "HAPPY";
+	        }
 }
--- src/test/java/com/demojunit/MoodAnalyzerTest.java
+++ src/test/java/com/demojunit/MoodAnalyzerTest.java
@@ -6,17 +6,23 @@ import org.junit.jupiter.api.Test;
 public class MoodAnalyzerTest {
 	@Test
 	public void givenSadMessage_WhenAnalysed_MessageShouldReturnSad() {
-		MoodAnalyzer moodAnalyser = new MoodAnalyzer();
-		String mood = moodAnalyser.analyseMood("sad Mood");
+		MoodAnalyzer moodAnalyser = new MoodAnalyzer("sad Mood");
+		String mood = moodAnalyser.analyseMood();
 		// assertTrue( true );
 		Assertions.assertEquals("SAD", mood);
 	}
 
 	@Test
 	public void givenAnyMoodMessage_WhenAnalysed_MessageShouldReturnHappy() {
-		MoodAnalyzer moodAnalyser = new MoodAnalyzer();
-		String mood = moodAnalyser.analyseMood("happy Mood");
+		MoodAnalyzer moodAnalyser = new MoodAnalyzer("happy Mood");
+		String mood = moodAnalyser.analyseMood();
 		Assertions.assertEquals("HAPPY", mood);
 	}
+	 @Test
+	    public void givenHappyMessage_WhenAnalysed_Message_ShouldReturnSad(){
+		 MoodAnalyzer moodAnalyser = new MoodAnalyzer(" happy Mood");
+	        String mood = moodAnalyser.analyseMood();
+	        Assertions.assertEquals("SAD", mood);
+	    }
 
 }
